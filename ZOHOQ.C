0afa68cde06340e72c3e956b66042b4981d36178
lavanyadav <54234884+lavanyadav@users.noreply.github.com>
2021-04-15 10:51:41 +0530

    Add files via upload

---
+++ ZOHOQ.C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char s[100];
+int l,j,i,k,m;
+l=strlen(s);
+scanf("%s",s);
+if(l%2==1)
+{       m=l/2;
+	for(i=l;i>0;i--)
+	{
+	for(j=0;j<i;j++)
+	{
+	printf(" ");
+	}
+	for(k=0;k<=l-i;k++)
+	{
+	if((k+m)<l)
+	{
+	printf("%c",s[k+m]);
+	}
+	else
+	{
+	printf("%c",s[k-m-1]);
+	}
+	}
+	printf("\n");
+	}
+}
+}
